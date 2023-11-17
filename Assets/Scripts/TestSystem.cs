a0a9c4e016a4a4a1b5241140a02ec0c0e86f2205
acz055 <jannes.habeck@haw-hamburg.de>
2023-11-17 18:37:38 +0100

    Init Commit

---
+++ Assets/Scripts/TestSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TestSystem : MonoBehaviour
+{
+
+    public TextMeshProUGUI TestText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestText.text = "Hello World!";
+    }
+}
